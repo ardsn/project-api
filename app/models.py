324fc4dcbb5493023e8b798b5e576cfa1a43f572
ardsn <rod_sn@hotmail.com>
2025-05-13 13:25:40 -0300

    feat: updated models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+from .utils import validate_cpf, standardize_numeric_string, standardize_email
 from typing import Iterable
 from django.db import models
 
@@ -16,7 +19,7 @@ class City(models.Model):
         ordering = ['name']
 
     def __str__(self):
-        return f"{self.name} - {self.state}"
+        return f"{self.name}/{self.state}"
 
 
 class Business(models.Model):
@@ -39,6 +42,7 @@ class Business(models.Model):
     public_phone = models.CharField(max_length=15)
     restricted_phone = models.CharField(max_length=15)
     email = models.EmailField(max_length=100)
+    schedule = models.JSONField()
     closed_on_holidays = models.BooleanField()
 
     class Meta:
@@ -66,4 +70,112 @@ class Business(models.Model):
             
             # Update the state field with the choices
             self._meta.get_field('state').choices = choices
+
     
+    def __str__(self):
+        return f"{self.name} | {self.category} | {self.city}/{self.state}"
+
+
+    def save(self, **kwargs):
+        self.name = self.name.title()
+        self.public_phone = standardize_numeric_string(self.public_phone)
+        self.restricted_phone = standardize_numeric_string(self.restricted_phone)
+        self.email = standardize_email(self.email)
+        super().save(**kwargs)
+
+
+
+class Customer(models.Model):
+    SOURCE_CHOICES = [
+        ("whatsapp", "whatsapp"),
+        ("website", "website")
+    ]
+
+    name = models.CharField(max_length=80)
+    birth_date = models.DateField(blank=True, null=True)
+    registered_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    registration_source = models.CharField(
+        max_length=50,
+        choices=SOURCE_CHOICES,
+        editable=False
+    )
+    cpf = models.CharField(
+        max_length=11,
+        validators=[validate_cpf],
+        unique=True
+    )
+    email = models.EmailField(max_length=60, blank=True, null=True)
+    phone = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
+    is_opt_in = models.BooleanField(default=False)
+
+    def save(self, **kwargs):
+        self.name = self.name.replace(" ", "").title()
+        self.registration_source = self.registration_source.lower()
+        self.email = standardize_email(self.email)
+        # Remove non numeric characters
+        self.phone = standardize_numeric_string(self.phone)
+        self.cpf = standardize_numeric_string(self.cpf)
+        super().save(**kwargs)
+
+
+class Service(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.DurationField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['business', 'name'],
+                name='unique_business_service'
+            )
+        ]
+
+
+    def __str__(self):
+        return f"{self.business.name} | {self.name}"
+
+    def save(self, **kwargs):
+        self.name = self.name.capitalize()
+        super().save(**kwargs)
+
+
+# # TODO: Finish the model
+# class Calendar(models.Model):
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     business = models.ForeignKey(Business, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     date = models.DateTimeField()
+#     service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+
+class Professional(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    speciality = models.CharField(max_length=50)
+    email = models.EmailField(max_length=60)
+    phone = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
+    schedule = models.JSONField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['business', 'name'],
+                name='unique_business_professional'
+            )
+        ]
+
+    def save(self, **kwargs):
+        self.email = standardize_email(self.email)
+        self.speciality = self.speciality.capitalize()
+        self.name = self.name.title()
+        # Remove non numeric characters
+        self.phone = standardize_numeric_string(self.phone)
+        super().save(**kwargs)
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,11 @@
 import httpx
 import logging
 from typing import Any, Iterable
+from django.core.exceptions import ValidationError
+
 
 logger = logging.getLogger(__name__)
+
 
 def fetch_cities() -> Iterable[dict[str, Any]]:
     logger.info("Fetching Brazilian cities from IBGE...")
@@ -12,3 +15,32 @@ def fetch_cities() -> Iterable[dict[str, Any]]:
     response.raise_for_status()
     logger.info(f"Successfully fetched {len(response.json())} cities from IBGE")
     return response.json()
+
+
+def validate_cpf(cpf: str) -> None:
+    import re
+    # Remove all non-numeric characters
+    cpf: str = re.sub(r'\D', '', cpf)
+
+    # Check if it has 11 digits or all digits aren't equal
+    if len(cpf) != 11:
+        raise ValidationError("Invalid CPF! It must have 11 digits.")
+    
+    if cpf == cpf[0] * 11:
+        raise ValidationError("Invalid CPF! All digits are the same.")
+
+    # Validation of the verifier digits
+    for i in range(9, 11):
+        soma = sum(int(cpf[num]) * ((i+1) - num) for num in range(0, i))
+        digito = ((soma * 10) % 11) % 10
+        if digito != int(cpf[i]):
+            raise ValidationError("Invalid CPF! Verifier digits are invalid.")
+    return
+
+
+def standardize_numeric_string(value: str) -> str:
+    return "".join(filter(str.isdigit, value))
+
+
+def standardize_email(email: str) -> str:
+    return email.replace(" ", "").lower()
