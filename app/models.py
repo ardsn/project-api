fff42f573fde7719a146e165e31ee7df4ab536d6
ardsn <rod_sn@hotmail.com>
2025-05-15 18:31:03 -0300

    feat: update models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,20 @@
 from __future__ import annotations
-
+import enum
 from .utils import validate_cpf, standardize_numeric_string, standardize_email
 from typing import Iterable
 from django.db import models
+
+
+SOURCE_CHOICES = [
+    ("whatsapp", "whatsapp"),
+    ("website", "website")
+]
+
+class AppointmentStatus(enum.StrEnum):
+    SCHEDULED = "agendado"
+    CONFIRMED = "confirmado"
+    CANCELLED = "cancelado"
+    COMPLETED = "concluído"
 
 
 class City(models.Model):
@@ -13,7 +25,7 @@ class City(models.Model):
         constraints = [
             models.UniqueConstraint(
                 fields=['name', 'state'],
-                name='unique_city_by_state'
+                name='unique_name_state'
             )
         ]
         ordering = ['name']
@@ -44,12 +56,16 @@ class Business(models.Model):
     email = models.EmailField(max_length=100)
     schedule = models.JSONField()
     closed_on_holidays = models.BooleanField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
                 fields=['name', 'state', 'city', 'category'],
-                name='unique_business_by_category',
+                name='unique_name_category_state_city'
             )
         ]
 
@@ -86,32 +102,35 @@ class Business(models.Model):
 
 
 class Customer(models.Model):
-    SOURCE_CHOICES = [
-        ("whatsapp", "whatsapp"),
-        ("website", "website")
-    ]
-
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
     birth_date = models.DateField(blank=True, null=True)
-    registered_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     registration_source = models.CharField(
         max_length=50,
         choices=SOURCE_CHOICES,
         editable=False
     )
     cpf = models.CharField(
-        max_length=11,
-        validators=[validate_cpf],
-        unique=True
+        max_length=14,
+        validators=[validate_cpf]
     )
     email = models.EmailField(max_length=60, blank=True, null=True)
     phone = models.CharField(max_length=15)
     is_active = models.BooleanField(default=True)
     is_opt_in = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['business', 'cpf'],
+                name='unique_customer_business'
+            )
+        ]
 
     def save(self, **kwargs):
-        self.name = self.name.replace(" ", "").title()
+        self.name = self.name.title()
         self.registration_source = self.registration_source.lower()
         self.email = standardize_email(self.email)
         # Remove non numeric characters
@@ -127,12 +146,14 @@ class Service(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration = models.DurationField()
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
                 fields=['business', 'name'],
-                name='unique_business_service'
+                name='unique_business_name'
             )
         ]
 
@@ -145,29 +166,25 @@ class Service(models.Model):
         super().save(**kwargs)
 
 
-# # TODO: Finish the model
-# class Calendar(models.Model):
-#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-#     business = models.ForeignKey(Business, on_delete=models.CASCADE)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     date = models.DateTimeField()
-#     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-
-
 class Professional(models.Model):
     business = models.ForeignKey(Business, on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
+    cpf = models.CharField(
+        max_length=14,
+        validators=[validate_cpf]
+    )
     speciality = models.CharField(max_length=50)
     email = models.EmailField(max_length=60)
     phone = models.CharField(max_length=15)
     is_active = models.BooleanField(default=True)
     schedule = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['business', 'name'],
+                fields=['business', 'cpf'],
                 name='unique_business_professional'
             )
         ]
@@ -178,4 +195,47 @@ class Professional(models.Model):
         self.name = self.name.title()
         # Remove non numeric characters
         self.phone = standardize_numeric_string(self.phone)
+        self.cpf = standardize_numeric_string(self.cpf)
         super().save(**kwargs)
+
+
+# TODO: Finish the model
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        (
+            AppointmentStatus.SCHEDULED.value,
+            AppointmentStatus.SCHEDULED.value
+        ),
+        (
+            AppointmentStatus.CANCELLED.value,
+            AppointmentStatus.CANCELLED.value
+        ),
+        (
+            AppointmentStatus.COMPLETED.value,
+            AppointmentStatus.COMPLETED.value
+        ),
+    ]
+
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    professional = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=AppointmentStatus.SCHEDULED.value)
+    source = models.CharField(max_length=50, choices=SOURCE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['business', 'professional', 'datetime', 'customer', 'service', 'status'],
+                name='unique_business_professional_datetime_customer_service_status'
+            ) 
+        ]
+
+    def save(self, **kwargs):
+        self.status = self.status.lower()
+        self.source = self.source.lower()
+        super().save(**kwargs)
+        
